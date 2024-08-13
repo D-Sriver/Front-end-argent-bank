6e9be063bfd3f24a519efef708690f5fc37e16e2
D.Sriver <duez.s@proton.me>
2024-08-13 17:17:53 +0200

    🎉 Initial commit + integration

---
+++ Argent-bank-front/src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/pages/Home/Home';
+import SignIn from './components/pages/SignIn';
+import User from './components/pages/User';
+
+export default function App() {
+	return (
+		<Router>
+			<div className="app">
+				<Header />
+				<main>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/sign-in" element={<SignIn />} />
+						<Route path="/user" element={<User />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
+	);
+}
